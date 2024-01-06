297f7f35508a64483b95e643f9d7f3d41d66075c
tiago1820 <tiago.zdo@gmail.com>
2024-01-05 23:52:10 -0300

    Express API with TypeScript

--- 00-hola/index.js
+++
@@ -1,5 +0,0 @@
-function saludar(nombre) {
-    console.log("\u00A1Hola, ".concat(nombre, "!"));
-}
-var miNombre = 'Juan';
-saludar(miNombre);
--- 00-hola/index.ts
+++
@@ -1,6 +0,0 @@
-function saludar(nombre: string){
-    console.log(`¡Hola, ${nombre}!`);
-}
-
-const miNombre = 'Juan';
-saludar(miNombre);
