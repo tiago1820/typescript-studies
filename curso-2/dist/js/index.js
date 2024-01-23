e39c412e22aec7f521ccd4d85cff3e6074510aa5
tiago1820 <tiago.zdo@gmail.com>
2024-01-23 10:19:43 -0300

    New course

---
+++ curso-2/dist/js/index.js
@@ -0,0 +1,13 @@
+"use strict";
+function suma(a, b) {
+    return a + b;
+}
+console.log(suma(1, 3));
+const nombre = "Miguel";
+const persona = {
+    name: "Pepe",
+    age: 30
+};
+console.log(persona.name);
+// ignore el tipo
+let anyValue = "hola";
