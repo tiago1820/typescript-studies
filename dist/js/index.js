c2748ee563a34c196099aa2b185dd69d347498ca
tiago1820 <tiago.zdo@gmail.com>
2024-01-23 09:10:00 -0300

    decorator

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,4 +1,10 @@
 "use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
 class User {
     name;
     role;
@@ -43,4 +49,24 @@ class SuperCar extends Car {
     }
 }
 const a4 = new SuperCar("Audi", 4, 2.0);
+console.log(a4);
 a4.showBrand();
+function BaseParameters() {
+    return function (constructor) {
+        return class extends constructor {
+            id = Math.random();
+            createdAt = new Date();
+        };
+    };
+}
+let Person = class Person {
+    name;
+    constructor(name) {
+        this.name = name;
+    }
+};
+Person = __decorate([
+    BaseParameters()
+], Person);
+const sam = new Person("Sam");
+console.log(sam);
