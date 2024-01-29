ce99cba02ea03d2036a2176e44a98eb0d9f7c597
tiago1820 <tiago.zdo@gmail.com>
2024-01-29 07:08:06 -0300

    typescript api

---
+++ express-typescript/src/services/diaryServices.ts
@@ -0,0 +1,7 @@
+import { DiaryEntry } from './types'
+import diaryData from './diaries.json'
+
+const diaries: DiaryEntry[] = diaryData as DiaryEntry[]
+export const getEntries = (): DiaryEntry[] => diaries
+
+export const addEntry = (): undefined => undefined;
